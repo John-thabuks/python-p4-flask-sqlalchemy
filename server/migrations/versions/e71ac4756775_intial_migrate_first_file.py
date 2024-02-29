1a63346fa5ef78ae93ce3335d2c818d04185a070
John-thabuks <thabuksjohn@gmail.com>
2024-02-29 08:33:18 +0300

    fix(core) migrations

---
+++ server/migrations/versions/e71ac4756775_intial_migrate_first_file.py
@@ -0,0 +1,24 @@
+"""Intial migrate_first file
+
+Revision ID: e71ac4756775
+Revises: 
+Create Date: 2024-02-29 08:27:43.721057
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e71ac4756775'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
